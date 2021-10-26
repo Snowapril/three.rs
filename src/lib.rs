992c00f68adf6e5176895b744ac64347b38e0e12
snowapril <sinjihng@gmail.com>
2021-10-26 20:07:04 +0900

    feat(main): add debug logging utils

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,13 @@ extern "C" {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+#[allow(unused_macros)]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
